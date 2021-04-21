3868d120d4ab2f24aa78b928e4af665bbf02582e
seankhu99 <77817725+seankhu99@users.noreply.github.com>
2021-04-20 21:48:38 -0500

    added marker image

--- js/script.js
+++ js/script.js
@@ -1,8 +1,32 @@
+var map;
+
 function initMap() {
-  var location = {lat: 41.75804020976093, lng: -88.214985292258522};
-  var map = new google.maps.Map(document.getElementById("map"), {
-    center: location,
-    zoom: 10
+  map = new google.maps.Map(document.getElementById("map"), {
+    center: { lat: 41.75804020976093, lng: -88.214985292258522 },
+    zoom: 18,
+  });
+
+  //html stuff here
+  const IOInfo = '<div class="googleMapsInfo"><h1>Round 1</h1><p>Round 1 is an entertainment franchise originally from Japan. If you love Japanese arcade games, Round 1 is unrivaled in its offerings!</p></div>';
+  //icons
+  const mask = {
+    url: "images/arcade.jpg",
+    scaledSize: new google.maps.Size(36, 36)
   };
-  
+  //markers, 
+  const round1 = new google.maps.Marker ({
+    position: {lat: 41.759341307668706, lng: -88.2115829918256},
+    map,
+    icon: mask,
+    animation: google.maps.Animation.DROP,
+    title: "Round 1",
+  });
+  //windows
+  const round1Window = new google.maps.InfoWindow ({
+    content: IOInfo,
+  })
+  //click listeners
+  round1.addListener("click", () => {
+    round1Window.open(map, round1);
+  })
 }
