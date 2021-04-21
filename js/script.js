1725a9ce2dc91927164958558713ade5c735df39
seankhu99 <77817725+seankhu99@users.noreply.github.com>
2021-04-20 20:52:22 -0500

    fixed script ordering

---
+++ js/script.js
@@ -0,0 +1,8 @@
+function initMap() {
+  var location = {lat: 41.75804020976093, lng: -88.214985292258522};
+  var map = new google.maps.Map(document.getElementById("map"), {
+    center: location,
+    zoom: 10
+  };
+  
+}
